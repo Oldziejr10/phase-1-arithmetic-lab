7984a7545ea42f247e8373405db2f4217fd1aa26
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-05-18 23:46:21 -0400

    commit tree 4e252f96861bf83c7ceabc515e0513338b5a2dd3

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const multiply = 31 * 2;
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const num3 = 12
+const num4 = 3
+
+const mod = num3 / num4; 
+
+const max = (5,10,15,20) 
+Math.max(max);
+
+const random = Math.floor(Math.random() * 10) + 1;
